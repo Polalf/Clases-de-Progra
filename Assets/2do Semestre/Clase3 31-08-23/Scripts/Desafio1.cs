b0227b7f1b1913b0593639ccb1c1b1bca5b936a9
Polalf <103451330+Polalf@users.noreply.github.com>
2023-09-01 00:41:15 -0400

    Avance desafio

--- Assets/2do Semestre/Clase3 31-08-23/Scripts/Desafio1.cs
+++ Assets/2do Semestre/Clase3 31-08-23/Scripts/Desafio1.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Desafio1 : MonoBehaviour
 {
-    [SerializeField] Queue<Transform> positions = new Queue<Transform>(10);
+    [SerializeField] Queue<Vector3> positions = new Queue<Vector3>(10);
+    bool isMoving;
+    [SerializeField] float seconds;
+    [SerializeField] AnimationCurve curve;
     void Start()
     {
-        
+        isMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        if(Input.GetMouseButtonDown(0))
+        {
+            positions.Enqueue(mousePos);
+            if (!isMoving)
+            {
+                StartCoroutine(Moverse());
+            }
+        }
+    }
+
+    IEnumerator Moverse()
+    {
+        isMoving = true;
+        int x = 0;
+        while (x < positions.Count)
+        {
+            
+            Vector3 b = positions.Dequeue();
+            for (float i = 0; i < seconds; i += Time.deltaTime)
+            {
+                transform.DOMove(b, seconds);
+                yield return null;
+            }
+
+        }
+        isMoving = false;
     }
 }
