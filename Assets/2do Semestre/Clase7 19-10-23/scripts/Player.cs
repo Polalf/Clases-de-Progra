6aea50cb7ec1a3c1151ed4dff94c39d3f3d0e59b
a <alfonse2508@gmail.com>
2023-10-19 11:38:10 -0300

    Fmod 19-10-23

--- Assets/2do Semestre/Clase7 19-10-23/scripts/Player.cs
+++ Assets/2do Semestre/Clase7 19-10-23/scripts/Player.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using FMODUnity;
 
 public class Player : Character
 {
+    [SerializeField] private StudioEventEmitter jump, shoot, boom;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z)) jump.Play();
+        if(Input.GetKeyDown(KeyCode.X)) shoot.Play();
+        if(Input.GetKeyDown(KeyCode.C)) boom.Play();
+    }
     public override void Die()
     {
         SceneManager.LoadScene(2);
